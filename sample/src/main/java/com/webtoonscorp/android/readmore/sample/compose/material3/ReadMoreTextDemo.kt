0a8a54e5da4d61dde4e818eb1315b4f2ad3cd898
Sungyong An <sungyong.an@webtoonscorp.com>
2022-06-02 20:12:01 +0900

    Updates to Compose 1.2.0-beta03

--- sample/src/main/java/com/webtoonscorp/android/readmore/sample/compose/material3/ReadMoreTextDemo.kt
+++ sample/src/main/java/com/webtoonscorp/android/readmore/sample/compose/material3/ReadMoreTextDemo.kt
@@ -33,9 +33,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -58,7 +58,7 @@ import com.webtoonscorp.android.readmore.sample.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReadMoreTextDemo() {
-    val scrollBehavior = remember { TopAppBarDefaults.pinnedScrollBehavior() }
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarScrollState())
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {

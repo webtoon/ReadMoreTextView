3c770e72b439c822ff06ce6c5e62a650140b6acd
Sungyong An <sungyong.an@webtoonscorp.com>
2024-10-16 14:26:33 +0900

    Updates to Compose 1.8.0-alpha03

--- build-logic/convention/src/main/kotlin/com/webtoonscorp/android/readmore/buildlogic/Versions.kt
+++ build-logic/convention/src/main/kotlin/com/webtoonscorp/android/readmore/buildlogic/Versions.kt
@@ -2,6 +2,6 @@ package com.webtoonscorp.android.readmore.buildlogic
 
 object Versions {
     const val minSdk = 21
-    const val compileSdk = 34
+    const val compileSdk = 35
     const val targetSdk = 33
 }
